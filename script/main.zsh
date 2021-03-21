649d5d6b4dd16a0feeb2a8b5b345d2a87276824c
asa1253 <asa1253@live.com>
2021-03-21 21:21:18 +0800

    update

--- script/main.zsh
+++ script/main.zsh
@@ -34,37 +34,15 @@ EOF
   printf "123\n123" | passwd aur-build
   print "aur-build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
   chown -R aur-build:aur-build ~aur-build
-
-  LOG "Initing GPG"
-  rm -fr /etc/pacman.d/gnupg
-  pacman-key --init
-  pacman-key --populate archlinux
-  pacman-key --recv-keys $GPGKEY --keyserver hkp://ipv4.pool.sks-keyservers.net:11371
-  pacman-key --lsign-key $GPGKEY
-
-  LOG "Importing GPG"
-  sudo -u aur-build gpg --import --batch --yes ${0:A:h}/data/private.key
-  shred --remove ${0:A:h}/data/private.key
-  cat > ~aur-build/.gnupg/gpg-agent.conf <<EOF
-default-cache-ttl 7200
-max-cache-ttl 31536000
-allow-preset-passphrase
 EOF
   chown aur-build ~aur-build/.gnupg/gpg-agent.conf
   sudo -u aur-build gpg-connect-agent "RELOADAGENT" /bye
-
-  local keygrip=$(grep grp --max-count 1 <(
-     grep $GPGKEY -A 3 <(
-       sudo -u aur-build gpg --batch --with-colons --with-keygrip --list-secret-keys $GPGKEY
-  )))
-  keygrip=$keygrip[(s|:|w)2]
-  sudo -u aur-build /usr/lib/gnupg/gpg-preset-passphrase -c $keygrip < ${0:A:h}/data/private.passphrase
 
   LOG "Initing repo"
   mkdir -p ~aur-build/.cache/{pikaur/{build,pkg},aur}
   chown -R aur-build:aur-build ~aur-build/.cache/{pikaur/{build,pkg},aur}
   if [[ ! -f ~aur-build/.cache/pikaur/pkg/$REPO_NAME.db.tar.gz ]]; then
-    sudo -u aur-build repo-add -n -p -s -k $GPGKEY \
+    sudo -u aur-build repo-add -n -p -s -k  \
          ~aur-build/.cache/pikaur/pkg/$REPO_NAME.db.tar.gz
   fi
 
@@ -88,7 +66,7 @@ function build_repo() {
   local -a new_packages=(${new_packages:|packages})
   if (( $#new_packages )); then
     LOG "There are $#new_packages new packages"
-    sudo -u aur-build repo-add -n -p -s -k $GPGKEY \
+    sudo -u aur-build repo-add -n -p -s -k \
          ~aur-build/.cache/pikaur/pkg/$REPO_NAME.db.tar.gz \
          $new_packages
   else
@@ -99,14 +77,14 @@ function build_repo() {
 function deploy() {
   LOG "Uploading to server"
   rsync -avzr --delete -e 'ssh -i ./data/deploy_key -o StrictHostKeyChecking=no' \
-        ~aur-build/.cache/pikaur/pkg/ $SERVER
+        ~aur-build/.cache/pikaur/pkg/ 
 }
 
 function remove_package() {
   LOG "Revoming package $1"
   setopt local_options null_glob
   [[ -d ~aur-build/.cache/aur/$1 ]] && rm -rdf ~aur-build/.cache/aur/$1
-  sudo -u aur-build repo-remove -s -k $GPGKEY ~aur-build/.cache/pikaur/pkg/$REPO_NAME.db.tar.gz $1 \
+  sudo -u aur-build repo-remove -s -k  ~aur-build/.cache/pikaur/pkg/$REPO_NAME.db.tar.gz $1 \
     || LOG "Cannot found $1 in database"
   for file in ~aur-build/.cache/pikaur/pkg/$1-*.pkg.tar.*; do
     rm -f $file
